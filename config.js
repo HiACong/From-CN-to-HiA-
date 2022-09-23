527b0574873decee0a4e9cba630ca6de97bf218d
HiACong <114237806+HiACong@users.noreply.github.com>
2022-09-23 20:44:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Lời tỏ tình",
+    introTitle: 'Hải Anh Phạm',
+    introDesc: `Cưng cửng cừng cưng của anh
+    Hôm nay đẹp trời quá cưng nhỉ
+    Lét mi sâu diu dơ bất ngờ mà anh bảo từ trước nha cưng
+    Are u ready??,
+    btnIntro: '^^GoGoGoGo^^',
+    title: 'Hải Anh à. Có thể định mệnh đã cho t và m đến với nhau kể từ khi lần đầu t gặp m rồi phải không nhỉ 🥰',
+    desc: 'Điều tuyệt vời nhất cũng như là niềm vui lớn lao nhất của t kể từ khi bước chân vào giảng đường Đại học cho đến giờ đó chính là sự có mặt của m bên cạnh t đó. Thực sự là chúng mình đến với nhau không hề qua những lời tán tỉnh mà hoàn toàn xuất phát từ một tình bạn đẹp, và giờ đó là một tình yêu thật đẹp phải không nhỉ <33. Mình đã cùng nhau trải qua rất nhiều các kỉ niệm vui cũng có, buồn cũng có nhưng tuyệt vời nhất vẫn là mình vẫn luôn bên nhauuu. Bao cung đường mình bên nhau trên con xe của t quả là một hành trình dài mà có thể chúng mình không bao giờ muốn dừng lại nhỉ. Mình còn có những lần ôm nhau thật chặt sau những lần xa nhau và nhớ ơi là nhớ nè, những lần ngồi hồ chiu quá không muốn về nè...Còn rất rất nhiều kỉ niệm rất đẹp khác mình từng trải qua, nhưng từ trước đến giờ có một điều t vẫn chưa dám và chưa có cơ hộ để nói với m. 
+    T thích m lắm. T thực sự thích m.
+    Anh yêu emmmmm. Em đồng ý làm người yêu anh nhé.',
+    btnYes: 'Đồng ý luônnnn',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Hãy nói gì đó để trả lời câu hỏi của t đi nàooo',
+    btnReply: 'Em cũng yêu anh nhiều lắmmmm',
+    reply: 'Em cũng yêu anh nhiều lắmmmm',
+    mess: 'Anh biết mà hihi 🥰. Yêu Em nhiều nhiều lắm luônnn 😘😘',
+    messDesc: 'Hẹn cưng tối cuối tuần này mình đi overnight nhaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/profile.php?id=100015985060515
 }
