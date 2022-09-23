81bf837ebfcfcd4418a5d97f01e459e2a3c641af
HiACong <114237806+HiACong@users.noreply.github.com>
2022-09-23 23:10:26 +0700

    Update config.js
    Signed-off-by: HiACong <114237806+HiACong@users.noreply.github.com>

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ const CONFIG = {
     Are u ready??,
     btnIntro: '^^GoGoGoGo^^',
     title: 'Hải Anh à. Có thể định mệnh đã cho t và m đến với nhau kể từ khi lần đầu t gặp m rồi phải không nhỉ 🥰',
-    desc: 'Điều tuyệt vời nhất cũng như là niềm vui lớn lao nhất của t kể từ khi bước chân vào giảng đường Đại học cho đến giờ đó chính là sự có mặt của m bên cạnh t đó. Thực sự là chúng mình đến với nhau không hề qua những lời tán tỉnh mà hoàn toàn xuất phát từ một tình bạn đẹp, và giờ đó là một tình yêu thật đẹp phải không nhỉ <33. Mình đã cùng nhau trải qua rất nhiều các kỉ niệm vui cũng có, buồn cũng có nhưng tuyệt vời nhất vẫn là mình vẫn luôn bên nhauuu. Bao cung đường mình bên nhau trên con xe của t quả là một hành trình dài mà có thể chúng mình không bao giờ muốn dừng lại nhỉ. Mình còn có những lần ôm nhau thật chặt sau những lần xa nhau và nhớ ơi là nhớ nè, những lần ngồi hồ chiu quá không muốn về nè...Còn rất rất nhiều kỉ niệm rất đẹp khác mình từng trải qua, nhưng từ trước đến giờ có một điều t vẫn chưa dám và chưa có cơ hộ để nói với m. 
-    T thích m lắm. T thực sự thích m.
-    Anh yêu emmmmm. Em đồng ý làm người yêu anh nhé.',
+    desc: 'rrr.',
     btnYes: 'Đồng ý luônnnn',
     btnNo: 'Không nha :3',
     question: 'Hãy nói gì đó để trả lời câu hỏi của t đi nàooo',
